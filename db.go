c71bf7ee064ee10582312d674c0d134bc40e6ddb
Silas Rech <silas@panascais.net>
2019-03-30 13:24:23 +0100

    Up the txt limit to 128

--- db.go
+++ db.go
@@ -164,7 +164,7 @@ func (d *acmedb) handleDBUpgradeTo1() error {
 func (d *acmedb) NewTXTValuesInTransaction(tx *sql.Tx, subdomain string) error {
 	var err error
 	instr := fmt.Sprintf("INSERT INTO txt (Subdomain, LastUpdate) values('%s', 0)", subdomain)
-	for i := 1; i <= 16; i++ {
+	for i := 1; i <= 128; i++ {
 	    _, err = tx.Exec(instr)
 	}
 	return err
@@ -253,7 +253,7 @@ func (d *acmedb) GetTXTForDomain(domain string) ([]string, error) {
 	domain = sanitizeString(domain)
 	var txts []string
 	getSQL := `
-	SELECT Value FROM txt WHERE Subdomain=$1 LIMIT 16
+	SELECT Value FROM txt WHERE Subdomain=$1 LIMIT 128
 	`
 	if Config.Database.Engine == "sqlite3" {
 		getSQL = getSQLiteStmt(getSQL)
